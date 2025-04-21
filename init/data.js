e80ccc8cf4f8594bd43bfe9092869c2224827d0f
Yash Rahangdale <rahangdaleyr@rknec.edu>
2025-04-21 21:24:16 +0530

    add

--- init/data.js
+++ init/data.js
@@ -11,18 +11,7 @@ const sampleListings = [
       location: "Malibu",
       country: "United States",
     },
-    {
-      title: "Modern Loft in Downtown",
-      description:
-        "Stay in the heart of the city in this stylish loft apartment. Perfect for urban explorers!",
-      image: {
-        filename: "listingimage",
-        url: "https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
-      },
-      price: 1200,
-      location: "New York City",
-      country: "United States",
-    },
+    
     {
       title: "Mountain Retreat",
       description:
@@ -333,6 +322,18 @@ const sampleListings = [
       },
       price: 4000,
       location: "Aspen",
+      country: "United States",
+    },
+    {
+      title: "Modern Loft in Downtown",
+      description:
+        "Stay in the heart of the city in this stylish loft apartment. Perfect for urban explorers!",
+      image: {
+        filename: "listingimage",
+        url: "https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      },
+      price: 1200,
+      location: "New York City",
       country: "United States",
     },
     {
