f540a56284b1d6e4d55687b4d51e4386b89eeabb
Yash Rahangdale <rahangdaleyr@rknec.edu>
2025-08-08 22:16:42 +0530

    add

--- classroom/server.js
+++ classroom/server.js
@@ -3,10 +3,11 @@ const app=  express();
 const users= require('./routes/user.js');
 const posts= require('./routes/post.js');
 const cookieParser = require('cookie-parser');
+const flash = require("connect-flash");
 
 const session = require('express-session');
 
-// app.use(cookieParser("secretcode"));
+//app.use(cookieParser("secretcode"));
 
 // app.get("/getsignedcookie",(req,res)=>{
 //     res.cookie("made-in","India",{signed:true});
@@ -34,11 +35,37 @@ const session = require('express-session');
 
 //express session
 app.use(session({
-    secret:"mysupersecretstring"
+    secret:"mysupersecretstring",
+    resave:false,  // it is use for avoid terminal warning
+    saveUninitialized:true // iska bhi kam   wahi hai
 }));
 
-app.get("/test", (req, res) => {
-    res.send("Testing session");
+app.use(flash());  // it is use for the store and see one time msg
+
+// app.get("/test", (req, res) => {
+//     res.send("Testing session");
+// });
+
+// app.get("/reqcount", (req, res) => {
+//     if(req.session.count) {
+//         req.session.count++;
+//     }else{
+//     req.session.count = 1;
+//     }
+//     res.send(`you sent a request ${req.session.count}times`)
+// });
+
+app.get("/register", (req, res) => {
+    let{name = "anonymous"} = req.query;
+    req.session.name = name;
+    console.log(req.session.name);
+    req.flash("success", "Registration successful!");
+    res.redirect("/hello");
+});
+
+app.get("/hello", (req, res) => {
+
+    res.send(`Hello,${req.session.name}`);
 });
 
 app.listen(3000, () => {
